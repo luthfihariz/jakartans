f8875705db35835a5bab11e64b4f4813a5869b2d
subhanshuja <subhan.nooriansyah@gmail.com>
2014-04-26 19:41:23 +0700

    bismillah clean my repository again

---
+++ src/com/jakartans/adapter/TrayekFullAdapter.java
@@ -0,0 +1,33 @@
+package com.jakartans.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class TrayekFullAdapter extends BaseAdapter {
+
+	Context mContext;
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
